0fbc5b0bd7ce5c68fd8db2f8abf8adcc99df727a
Levi Zitting <levizitting5663@gmail.com>
2018-10-14 19:54:04 -0500

    Basic CRUD for users. There is no password hashing right now, coming soon

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,32 +1,48 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 import { CreateUserDto } from './create-user.dto';
+import { User } from './user.entity';
 
 @Controller('user')
 export class UserController {
   @Get()
-  index(@Query() query) {
-
+  async index(@Query() query) {
+    return await User.find();
   }
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
+    const user = new User();
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
 
+    return await user.save();
   }
 
   @Get(':id')
-  show(@Param('id') id) {
-
+  async show(@Param('id') id) {
+    return await User.findOne({ id });
   }
 
   @Put(':id')
-  update(@Param('id') id, @Body() updateUserDto) {
+  async update(@Param('id') id, @Body() updateUserDto: CreateUserDto) {
+    const user = await User.findOne({ id });
 
+    user.firstName = updateUserDto.firstName;
+    user.lastName = updateUserDto.lastName;
+    user.email = updateUserDto.email;
+    user.password = updateUserDto.password;
+
+    return await user.save();
   }
 
   @Delete(':id')
-  destroy(@Param('id') id) {
+  async destroy(@Param('id') id) {
+    const user = await User.findOne({ id });
 
+    return await user.remove();
   }
 
 }
